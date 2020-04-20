910b20650bb40610da33a4bba9380d2b57239f9d
Adam Jeliński <ajelinski@antmicro.com>
2020-04-20 09:17:38 +0200

    Remove patches pushed to renode master; test on Windows

--- renode/prescript..sh
+++ renode/prescript..sh
@@ -14,13 +14,11 @@ cd ..
 cat meta.yaml
 mv renode/tools/packaging/conda/* .
 
-sed -i.b 's/name: renode/name: renode-travis/' meta.yaml
-sed -i.b "s/{{GIT_DESCRIBE_TAG}}/$DESCRIBE_TAG/" meta.yaml
-sed -i.b 's/git_url: .*/path: renode/' meta.yaml
-sed -i.b 's/git_rev: /# git_rev: /' meta.yaml
-rm meta.yaml.b
-patch meta.yaml meta_requirements.patch
-patch build.sh build_sed.patch
+sed -i.bak 's/name: renode/name: renode-travis/' meta.yaml
+sed -i.bak "s/{{GIT_DESCRIBE_TAG}}/$DESCRIBE_TAG/" meta.yaml
+sed -i.bak 's/git_url: .*/path: renode/' meta.yaml
+sed -i.bak 's/git_rev: /# git_rev: /' meta.yaml
+rm meta.yaml.bak
 patch build.sh build_without_gui.patch
 patch renode/tools/common.sh tools_common.patch
 
