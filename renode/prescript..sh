4b6345fe38fd44f17ab031afdb4d3624a8b6f40b
Adam Jeliński <ajelinski@antmicro.com>
2020-04-22 09:43:20 +0200

    Test all OSs

--- renode/prescript..sh
+++ renode/prescript..sh
@@ -14,20 +14,22 @@ cd ..
 cat meta.yaml
 mv renode/tools/packaging/conda/* .
 
+# Test general Renode fixes
 patch meta.yaml win-python-version.patch
+patch activate.sh limit-libdl-finding-to-linux.patch
+
 sed -i.bak 's/name: renode/name: renode-travis/' meta.yaml
 sed -i.bak "s/{{GIT_DESCRIBE_TAG}}/$DESCRIBE_TAG/g" meta.yaml
 sed -i.bak 's/git_url: .*/path: renode/' meta.yaml
 sed -i.bak 's/git_rev: /# git_rev: /' meta.yaml
 rm meta.yaml.bak
-patch build.sh build_without_gui.patch
-patch renode/build.sh verbose_build.patch
-patch renode/tools/common.sh tools_common.patch
-patch bld.bat add_msbuilds-path_in_bld.patch
-if [[ `uname` != 'Linux' && `uname` != 'Darwin' ]]; then
+
+cat meta.yaml
+
+if [[ `uname` == 'Linux' || `uname` == 'Darwin' ]]; then
+  patch build.sh build_without_gui.patch
+  patch renode/tools/common.sh tools_common.patch
+else
   sed -i.bak 's/\/p:/-p:/g' renode/build.sh
   rm renode/build.sh.bak
 fi
-cat meta.yaml
-
-ls -l
