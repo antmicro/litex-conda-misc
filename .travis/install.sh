dc6635414b08bbb5be401ebb5ad246d7fbfb8520
Adam Jeliński <ajelinski@antmicro.com>
2020-05-07 08:41:15 +0200

    scripts: Allow packages to have their own condarc
    It can be single condarc for all platforms: $PACKAGE/condarc, but if
    $PACKAGE/condarc_$TRAVIS_OS_NAME exists, it will be used instead.
    
    Such file is used as a $CONDARC environment variable, which has higher
    priority than other configuration files, in case of any conflict.
    
    Channels from $CONDARC are appended to the top of the channels list.

--- .travis/install.sh
+++ .travis/install.sh
@@ -13,10 +13,20 @@ conda config --set always_yes yes --set changeps1 no
 conda install pexpect
 conda config --add channels litex-hub
 conda config --add channels antmicro
-for CHANNEL in $CONDA_CHANNELS; do
-	conda config --add channels $CHANNEL
-done
 conda config --add channels $(echo $TRAVIS_REPO_SLUG | sed -e's@/.*$@@')
+
+if [ -e $PACKAGE/condarc_$TRAVIS_OS_NAME ]; then
+	export CONDARC=$PACKAGE/condarc_$TRAVIS_OS_NAME
+elif [ -e $PACKAGE/condarc ]; then
+	export CONDARC=$PACKAGE/condarc
+fi
+
+# The CONDARC variable seems to be ignored by the conda-build
+# Therefore, it is used as the global configuration file
+if [ -e $CONDARC ]; then
+	cp $CONDARC $CONDA_PATH/condarc
+fi
+
 #conda clean -s --dry-run
 conda build purge
 #conda clean -s --dry-run
